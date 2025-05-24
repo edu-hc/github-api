3443f318d01a1f5130b145474834b7d7c16d0d04
edu-hc <edu.carvc@gmail.com>
2025-05-24 17:22:03 -0300

    init: repo init

---
+++ src/main/java/com/api/githubc/GithubcApplication.java
@@ -0,0 +1,13 @@
+package com.api.githubc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/githubc/GithubcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.githubc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
